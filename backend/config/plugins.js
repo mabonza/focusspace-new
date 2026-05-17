1c5442d8e23e8edd3efb75388315bd28a433240e
lady <lady@viva-vision.net>
2026-05-17 19:49:26 +0200

    Add Cloudinary upload provider for persistent file storage

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,6 +1,21 @@
 'use strict'
 
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   'users-permissions': {
     config: {
       jwt: {
