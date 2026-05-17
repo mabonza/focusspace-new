a60d86b99ffca9b9578ec9801b1da5c5625a9df0
lady <lady@viva-vision.net>
2026-05-17 20:06:13 +0200

    Allow Cloudinary image URLs in CSP for Strapi admin panel

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -3,7 +3,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
